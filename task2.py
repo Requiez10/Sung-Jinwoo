6dcea35144e2092a847fe467538df1a350a9cc50
REQUIEZ, JOHN RIVEN D. <167667538+Requiez10@users.noreply.github.com>
2025-02-18 10:44:28 +0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -9,9 +9,9 @@ class Student:
         print(f"\nHello, my name is {self.name}. I am {self.age} years old and I am studying {self.course}.")
 
 
-student1 = Student(name="Sung Jinwoo", age=20, course="Computer Science")
-student2 = Student(name="Gojo Satoru", age=22, course="Mathematics")
-student3 = Student(name="Sung Jinah", age=21, course="Physics")
+student1 = Student(name="Sung Jinwoo", age=20, course="Computer Engineering")
+student2 = Student(name="Gojo Satoru", age=22, course="Information Technology")
+student3 = Student(name="Sung Jinah", age=21, course="Education")
 
 
 student1.introduce()
